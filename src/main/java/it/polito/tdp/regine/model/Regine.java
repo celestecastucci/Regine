f969d2f68535dcf4441c3699e6a2938de3e9d50d
celes <celes@DESKTOP-04JUM8H>
2021-04-12 09:52:57 +0200

    Solzione funzionante (trova la prima sol. valida)

--- src/main/java/it/polito/tdp/regine/model/Regine.java
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -1,5 +1,6 @@
 package it.polito.tdp.regine.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Regine {
@@ -17,15 +18,83 @@ public class Regine {
 	//     [0, 2]
 	//            [0, 2, 1]
 	
-	private void cerca(List<Integer>parziale, int livello) {
+	private int N;
+	private List<Integer>soluzione;
+	/**
+	 * METODO CHE RESTITUISCE LISTA
+	 * @param N
+	 * @return
+	 */
+	public List<Integer> risolvi(int N){
+		this.N=N;
+		
+		//definisco ARRAYLIST PERCHE' SOTTO FACCIO .get
+		//per la linkedlist aggiunge ritardo
+		List<Integer>parziale= new ArrayList<Integer>();
+		this.soluzione=null; // cosi se non trovo nulla mi ritorna null
+		cerca(parziale,0);  //perchè al passo iniziale siamo a liv0
+		return this.soluzione;
+	}
+	
+	/**
+	 * IL METODO E' UN BOOLEAN :cosi la prima soluzione che trovo va bene e mi stoppo!!!
+	 * return true se ho soluzione oppure una delle chiamate ricorsive dice che lei ha in mano la soluzione
+	 * return false se non ho soluzione e neanche le chiamate ricorsive, continuiamo a cercare
+	 * @param parziale
+	 * @param livello
+	 * @return
+	 */
+	private boolean cerca(List<Integer>parziale, int livello) {
 		if(livello==N) {
 			// caso terminale
+			this.soluzione=new ArrayList<>(parziale);
+			return true;
 		} else {
-			for(int colonna=0; colonna<N; colonna++) {
-				// if la possa nella casella [livello][colonna] è valida
-				// se sì, aggiungi a parziale e fai ricorsione
+			for(int colonna=0; colonna<N; colonna++) {  //[0 6 4 7 ]
+				// if la pos nella casella [livello][colonna] è valida
+				// se sì, aggiungi a parziale e fai ricorsione 
+				//ricorsione con metodo cerca e incremento il livello
+			
+			if(posValida(parziale,colonna)) {
+				
+				/*List<Integer>parzialeNuovo= new ArrayList<>(parziale);
+				parzialeNuovo=parziale;
+				parzialeNuovo.add(colonna);
+				cerca(parzialeNuovo,livello+1);*/
+				
+				parziale.add(colonna);  //[0 6 4 7 xxxx] aggiungo colonna1
+				boolean trovato=cerca(parziale,livello+1); 
+				if(trovato)
+					return true;
+				parziale.remove(parziale.size()-1); //backtracking , poi tolgo1 perchè provo altre alternative
+				
 			}
+			}
+			return false;
 		}
+	}
+	/**
+	 * controllo se la posizione è valida
+	 * @param parziale
+	 * @param colonna
+	 * @return
+	 */
+	private boolean posValida(List<Integer> parziale, int colonna) {
+		int livello=parziale.size();
+		
+		//controlla se la posizione in cui sono è valida
+		if(parziale.contains(colonna))
+			return false;
+		//controlla le diagonali: confronta la posizione (livello,colonna) con (riga,colonna) 
+		//delle regine esistenti
+		for(int r=0; r<livello; r++) {
+			int c=parziale.get(r);
+			
+			if(r+c== livello+colonna || r-c==livello-colonna)
+				return false;
+			
+		}
+		return true;
 	}
 	
 	
